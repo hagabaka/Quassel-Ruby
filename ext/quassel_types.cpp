ffd99b2254153051761eb03285da38d2a2ec6a20
Yaohan Chen <yaohan.chen@gmail.com>
2012-02-23 13:52:19 -0500

    Register Quassel meta types in extension
    Now QDebug shows the names of custom Quassel types. Still not sure
    if it is getting the full data even on the extension side, and
    how to transfer it to Ruby side.

--- ext/quassel_types.cpp
+++ ext/quassel_types.cpp
@@ -5,6 +5,11 @@
 #include <QString>
 #include <QDebug>
 
+#include "types.h"
+#include "message.h"
+#include "network.h"
+#include "identity.h"
+
 VALUE QuasselTypes;
 extern "C" {
   void Init_QuasselTypes();
@@ -13,6 +18,46 @@ VALUE unserialize(VALUE self, VALUE string);
 
 
 void Init_QuasselTypes() {
+  // Copied from quassel.cpp Quassel::registerMetaTypes();
+  // FIXME if the function is made static in Quassel source, we can just call it
+
+  // Complex types
+  qRegisterMetaType<Message>("Message");
+  qRegisterMetaType<BufferInfo>("BufferInfo");
+  qRegisterMetaType<NetworkInfo>("NetworkInfo");
+  qRegisterMetaType<Network::Server>("Network::Server");
+  qRegisterMetaType<Identity>("Identity");
+  qRegisterMetaType<Network::ConnectionState>("Network::ConnectionState");
+
+  qRegisterMetaTypeStreamOperators<Message>("Message");
+  qRegisterMetaTypeStreamOperators<BufferInfo>("BufferInfo");
+  qRegisterMetaTypeStreamOperators<NetworkInfo>("NetworkInfo");
+  qRegisterMetaTypeStreamOperators<Network::Server>("Network::Server");
+  qRegisterMetaTypeStreamOperators<Identity>("Identity");
+  qRegisterMetaTypeStreamOperators<qint8>("Network::ConnectionState");
+
+  qRegisterMetaType<IdentityId>("IdentityId");
+  qRegisterMetaType<BufferId>("BufferId");
+  qRegisterMetaType<NetworkId>("NetworkId");
+  qRegisterMetaType<UserId>("UserId");
+  qRegisterMetaType<AccountId>("AccountId");
+  qRegisterMetaType<MsgId>("MsgId");
+
+  qRegisterMetaType<QHostAddress>("QHostAddress");
+
+  qRegisterMetaTypeStreamOperators<IdentityId>("IdentityId");
+  qRegisterMetaTypeStreamOperators<BufferId>("BufferId");
+  qRegisterMetaTypeStreamOperators<NetworkId>("NetworkId");
+  qRegisterMetaTypeStreamOperators<UserId>("UserId");
+  qRegisterMetaTypeStreamOperators<AccountId>("AccountId");
+  qRegisterMetaTypeStreamOperators<MsgId>("MsgId");
+
+  // Versions of Qt prior to 4.7 didn't define QVariant as a meta type
+  if(!QMetaType::type("QVariant")) {
+    qRegisterMetaType<QVariant>("QVariant");
+    qRegisterMetaTypeStreamOperators<QVariant>("QVariant");
+  }
+
   QuasselTypes = rb_define_module("QuasselTypes");
   rb_define_singleton_method(QuasselTypes, "unserialize", (VALUE (*)(...))unserialize, 1);
 }
